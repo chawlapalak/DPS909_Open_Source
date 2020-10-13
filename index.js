1f6ecd99e2e9b376b51d68bd14e681bd21214af5
Palak Chawla <chawla.palak19@gmail.com>
2020-10-13 00:17:31 -0400

    Support --all, --good, and --bad flags

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const program = require('commander');
 const fileHandler = require('./utils/fileHandler');
 program
     .version('0.0.1', '-v, -V, --version ', 'prints tool version') //user can use any of the command to know the version
-    .option('-f, --file <path>', 'Path to file');
+    .option('-f, --file <path>', 'Path to file')
+    .option('--all', 'All URLs')
+    .option('--good', 'Good URLs')
+    .option('--bad', 'Bad URLs');
 
 program.parse(process.argv);
 
 if (program.file) {
     console.log(`file: ${program.file}`);
-    fileHandler.readCheckFile(program.file);
+    const displayOption = program.bad ? 'bad' : ( program.good ? 'good' : 'all' );
+    fileHandler.readCheckFile(program.file, displayOption);
 }
 
 
--- utils/fileHandler.js
+++ utils/fileHandler.js
@@ -14,38 +14,39 @@ else{
 var urls = [];
 
 //this method will read the file and check if the url is matching the pattern
-const readCheckFile = (fileName) => {
+const readCheckFile = (fileName, displayOption) => {
     fs.readFile(fileName, 'utf8', (err, data) => {
         if(err) {
             console.log(err);
         }
         else {
             urls = data
-            .toString()
             .match(/(http|https)(:\/\/)([\w+\-&@`~#$%^*.=/?:]+)/gi); 
-            checkURL(urls);
+            checkURL(urls, displayOption);
         }
     } );
 }
 
 //this method will check the urls if they are working or not and then print the codes in assigned colors 
-const checkURL = (urlList) => {
+const checkURL = (urlList, displayOption) => {
+    const showBad = displayOption==='all' || displayOption==='bad';
+    const showGood = displayOption==='all' || displayOption==='good';
     urlList.forEach(async (url) => {
         await axios
             .head(url, { timeout: 1500})
             .then((res) => {
-                if(res.status === 200) {
+                if(res.status === 200 && showGood ) {
                     console.log(colors.green.bold(`GOOD , Status : ${res.status} ${url}`));
                 }
-                else {
+                else if(showBad){
                     console.log(colors.grey.bold(`UNKNOWN , Status : ${res.status} ${url}`));
                 }
             })
             .catch((err) => {
-                if(err.response === undefined) {
+                if(err.response === undefined && showBad) {
                 console.log(colors.grey.bold(`UNKNOWN , Status : UNKNOWN ${url}`));
                 }
-                else {
+                else if(showBad){
                     if(err.response.status === 400 || err.response.status === 404) {
                         console.log(colors.red.bold(`BAD , Status : ${err.response.status} ${url}`));
                     }
