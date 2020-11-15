8f47fb9d916b0548e8a7d45098045e77d46f7576
Palak Chawla <chawla.palak19@gmail.com>
2020-11-14 23:51:55 -0500

    added prettier

--- index.js
+++ index.js
@@ -1,44 +1,39 @@
 #!/usr/bin/env node
-const restHandler = require('./utils/restHandler');
-const program = require('commander');
-const fileHandler = require('./utils/fileHandler');
-
+const restHandler = require("./utils/restHandler");
+const program = require("commander");
+const fileHandler = require("./utils/fileHandler");
 
 program
-    .version('0.0.1', '-v, -V, --version ', 'prints tool version') //user can use any of the command to know the version
-    .option('-f, --file <path>', 'Path to file')
-    .option('--all', 'All URLs')
-    .option('--good', 'Good URLs')
-    .option('--bad', 'Bad URLs')
-    .option('-i, --ignore <path>', 'Path to file with URLs to ignore')
-    .option('-e, --endpoint <url>', 'URL to fetch post list');
+  .version("0.0.1", "-v, -V, --version ", "prints tool version") //user can use any of the command to know the version
+  .option("-f, --file <path>", "Path to file")
+  .option("--all", "All URLs")
+  .option("--good", "Good URLs")
+  .option("--bad", "Bad URLs")
+  .option("-i, --ignore <path>", "Path to file with URLs to ignore")
+  .option("-e, --endpoint <url>", "URL to fetch post list");
 
 program.parse(process.argv);
 
-if(program.ignore && !program.file)
-{
-    console.log('Error: Usage url-inspector -i <path> - f <filepath>');
+if (program.ignore && !program.file) {
+  console.log("Error: Usage url-inspector -i <path> - f <filepath>");
 }
-
 
 if (program.file) {
-    console.log(`file: ${program.file}`);
-    const displayOption = program.bad ? 'bad' : ( program.good ? 'good' : 'all' );        
-    fileHandler.readCheckFile(program.file,program.ignore, displayOption);    
-    
+  console.log(`file: ${program.file}`);
+  const displayOption = program.bad ? "bad" : program.good ? "good" : "all";
+  fileHandler.readCheckFile(program.file, program.ignore, displayOption);
 }
 
-if(program.endpoint){
-    console.log(`endpoint: ${program.endpoint}`);
-    restHandler.resFetch(program.endpoint).then(
-        res => {
-            fileHandler.readCheckFile('postsUrl.txt', undefined, 'all');
-        }
-    ).catch(err => (console.log(err)));
-   
+if (program.endpoint) {
+  console.log(`endpoint: ${program.endpoint}`);
+  restHandler
+    .resFetch(program.endpoint)
+    .then((res) => {
+      fileHandler.readCheckFile("postsUrl.txt", undefined, "all");
+    })
+    .catch((err) => console.log(err));
 }
 
 process.on("exit", function (code) {
-    return console.log(`About to exit with code ${code}`);
-  });
-
+  return console.log(`About to exit with code ${code}`);
+});
--- utils/fileHandler.js
+++ utils/fileHandler.js
@@ -1,124 +1,110 @@
-const result = require('dotenv').config();
-const fs = require('fs');
-const axios = require('axios'); //using axios for fetching
-const colors = require('colors'); //using colors for req of colors
-const cliColor = process.env.CLICOLOR || result.parsed.CLICOLOR ;         //using cliColor 
+const result = require("dotenv").config();
+const fs = require("fs");
+const axios = require("axios"); //using axios for fetching
+const colors = require("colors"); //using colors for req of colors
+const cliColor = process.env.CLICOLOR || result.parsed.CLICOLOR; //using cliColor
 
-if(cliColor === "0"){
-    colors.disable();
-}
-else{
-    colors.enable();
+if (cliColor === "0") {
+  colors.disable();
+} else {
+  colors.enable();
 }
 
 var urls = [];
 var urlsIgnore = [];
 
-
-
 //this method will read the file and check if the url is matching the pattern
-const readCheckFile = (fileName,ignoreFile,displayOption) => {
-    
-    if(ignoreFile)
-    {
-        console.log(`Ignore URLs file: ${ignoreFile}`);
-        checkValid(ignoreFile);
+const readCheckFile = (fileName, ignoreFile, displayOption) => {
+  if (ignoreFile) {
+    console.log(`Ignore URLs file: ${ignoreFile}`);
+    checkValid(ignoreFile);
+  }
+
+  fs.readFile(fileName, "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      urls = data.match(/(http|https)(:\/\/)([\w+\-&@`~#$%^*.=/?:]+)/gi);
+
+      checkURL(urls, urlsIgnore, displayOption);
     }
-        
-    fs.readFile(fileName, 'utf8', (err, data) => {
-        if(err) {
-            console.log(err);
+  });
+};
+
+//this method will check the urls if they are working or not and then print the codes in assigned colors
+const checkURL = (urlList, ignoreUrlList, displayOption) => {
+  const showBad = displayOption === "all" || displayOption === "bad";
+  const showGood = displayOption === "all" || displayOption === "good";
+  let urlListFinal = [];
+
+  //if ignore urls list is not empty than push each url to the final list if the  url is not included in the ignore urls file
+  if (ignoreUrlList != null) {
+    urlList.forEach((url) => {
+      if (!ignoreUrlList.includes(url)) {
+        urlListFinal.push(url);
+      }
+    });
+  } else {
+    urlListFinal = urlList;
+  }
+  urlListFinal.forEach(async (url) => {
+    await axios
+      .head(url, { timeout: 1500 })
+      .then((res) => {
+        if (res.status === 200 && showGood) {
+          console.log(
+            colors.green.bold(`GOOD , Status : ${res.status} ${url}`)
+          );
+        } else if (showBad) {
+          console.log(
+            colors.grey.bold(`UNKNOWN , Status : ${res.status} ${url}`)
+          );
         }
-        else {
-            urls = data
-            .match(/(http|https)(:\/\/)([\w+\-&@`~#$%^*.=/?:]+)/gi);            
-            
-            checkURL(urls, urlsIgnore, displayOption);            
+      })
+      .catch((err) => {
+        if (err.response === undefined && showBad) {
+          console.log(colors.grey.bold(`UNKNOWN , Status : UNKNOWN ${url}`));
+        } else if (showBad) {
+          if (err.response.status === 400 || err.response.status === 404) {
+            console.log(
+              colors.red.bold(`BAD , Status : ${err.response.status} ${url}`)
+            );
+          } else {
+            console.log(
+              colors.grey.bold(
+                `UNKNOWN , Status : ${err.response.status} ${url}`
+              )
+            );
+          }
         }
-    } );
-}
-
-//this method will check the urls if they are working or not and then print the codes in assigned colors 
-const checkURL = (urlList,ignoreUrlList, displayOption) => {
-    const showBad = displayOption==='all' || displayOption==='bad';
-    const showGood = displayOption==='all' || displayOption==='good';
-    let urlListFinal = [];
-    
-    //if ignore urls list is not empty than push each url to the final list if the  url is not included in the ignore urls file
-    if(ignoreUrlList != null)
-    {
-        
-       urlList.forEach((url) =>{
-           if(!ignoreUrlList.includes(url))
-           {
-                urlListFinal.push(url);
-           }
-       })
-
-    }
-    else
-    {
-        urlListFinal = urlList;
-    }
-    urlListFinal.forEach(async (url) => {
-        await axios
-            .head(url, { timeout: 1500})
-            .then((res) => {
-                if(res.status === 200 && showGood ) {
-                    console.log(colors.green.bold(`GOOD , Status : ${res.status} ${url}`));
-                }
-                else if(showBad){
-                    console.log(colors.grey.bold(`UNKNOWN , Status : ${res.status} ${url}`));
-                }
-            })
-            .catch((err) => {
-                if(err.response === undefined && showBad) {
-                console.log(colors.grey.bold(`UNKNOWN , Status : UNKNOWN ${url}`));
-                }
-                else if(showBad){
-                    if(err.response.status === 400 || err.response.status === 404) {
-                        console.log(colors.red.bold(`BAD , Status : ${err.response.status} ${url}`));
-                    }
-                    else {
-                    console.log(colors.grey.bold(`UNKNOWN , Status : ${err.response.status} ${url}`));
-                    }
-                }
-            })
-    })
-
-}
+      });
+  });
+};
 
 //function that checks if the ignore urls file is valid and returns a list of urls to ignore
-const checkValid = (fileName) => 
-{
-    
-    fs.readFile(fileName, 'utf8', (err, data) => {
-        if(err) {
-            console.log(err);
+const checkValid = (fileName) => {
+  fs.readFile(fileName, "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      invalid = data.match(/[\n][^(#|http|https)].+$/gm);
+      if (invalid != null) {
+        console.log(
+          `${fileName} include invalid links. Links should start with http:// or https://`
+        );
+        process.exit(1);
+      } else {
+        urlsIgnore = data.match(
+          /(http|https)(:\/\/)([\w+\-&@`~#$%^*.=/?:]+)/gi
+        );
+        if (urlsIgnore != null) {
+          console.log(`The following URLs will not be checked ${urlsIgnore}`);
         }
-        else {
-            invalid = data
-            .match(/[\n][^(#|http|https)].+$/gm);
-            if(invalid != null)
-            {
-                console.log(`${fileName} include invalid links. Links should start with http:// or https://`)
-                process.exit(1); 
-            }
-            else {
-                urlsIgnore = data.match(/(http|https)(:\/\/)([\w+\-&@`~#$%^*.=/?:]+)/gi); 
-                if(urlsIgnore != null)
-                {
-                    console.log(`The following URLs will not be checked ${urlsIgnore}`);                
-                }                
-            }
-            
-        }
-    } );
+      }
+    }
+  });
+};
 
-    
-}
-
-
-module.exports = {    
-    readCheckFile        
-}
+module.exports = {
+  readCheckFile,
+};
